5629818b2fdb4b3c6fba38fcba4877c06d248d2b
nikita091999 <manenikita400@gmail.com>
2025-01-23 11:43:26 +0530

    Create config1.py

---
+++ config1.py
@@ -0,0 +1,21 @@
+{
+    "wifi": {
+        "ssid": "DataMann_5G",
+        "password": "Datamann@2022"
+    },
+    "mqtt": {
+        "broker": "tunnel.datamann.in",
+        "port": 8883,
+        "user": "dipak",
+        "password": "Dipak@123"
+    },
+    "device_info":{
+        "device_id":"ES4007",
+        "c_code":"C-001",
+        "a_code":"A-001",
+        "s_code":"POWERCON-HO",
+        "s_topic":"STJ",
+        "device_group":"G-01",
+        "sensor_delay":2
+    }
+}
